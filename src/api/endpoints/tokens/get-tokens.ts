472b6715c68a6be0f9c659a29509e0eec8eb0c75
George Roman <30772943+georgeroman@users.noreply.github.com>
2022-02-04 19:47:25 +0200

    Update get-tokens.ts

--- src/api/endpoints/tokens/get-tokens.ts
+++ src/api/endpoints/tokens/get-tokens.ts
@@ -27,7 +27,7 @@ export const getTokensOptions: RouteOptions = {
         .valid("asc", "desc")
         .default("asc"),
       offset: Joi.number().integer().min(0).default(0),
-      limit: Joi.number().integer().min(1).max(20).default(20),
+      limit: Joi.number().integer().min(1).max(20).default(50),
     })
       .or("contract", "collection", "tokenSetId")
       .oxor("contract", "collection", "tokenSetId"),
