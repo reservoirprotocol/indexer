639046de9451a1df0be91cd6dec9cbdbd58587b8
dikkeduif <donald.dewulf@gmail.com>
2022-02-11 05:17:36 -0500

    Add better swagger documentation

--- src/api/endpoints/apikeys/post-api-key.ts
+++ src/api/endpoints/apikeys/post-api-key.ts
@@ -7,11 +7,16 @@ export const postApiKey: RouteOptions = {
   description: "Create a new API key",
   notes: 'The API key can be used optionally in every route, set it as a request header **x-api-key**',
   tags: ["api", "apikeys"],
+  plugins: {
+    'hapi-swagger': {
+      payloadType: 'form'
+    }
+  },
   validate: {
     payload: Joi.object({
-      appName: Joi.string().required(),
-      email: Joi.string().email().required(),
-      website: Joi.string().uri().required()
+      appName: Joi.string().required().description('The name of the app'),
+      email: Joi.string().email().required().description('Your e-mail address so we can reach you'),
+      website: Joi.string().uri().required().description('The website of your project')
     }),
   },
   response: {
