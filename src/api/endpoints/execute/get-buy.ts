b85d997957532eeaabc2eed3e37d97b463e9ec48
George Roman <george.roman.99@gmail.com>
2022-02-14 15:39:35 +0200

    Fix

--- src/api/endpoints/execute/get-buy.ts
+++ src/api/endpoints/execute/get-buy.ts
@@ -34,7 +34,7 @@ export const getExecuteBuyOptions: RouteOptions = {
           action: Joi.string().required(),
           description: Joi.string().required(),
           status: Joi.string().valid("complete", "incomplete").required(),
-          kind: Joi.string().valid("transaction").required(),
+          kind: Joi.string().valid("transaction", "confirmation").required(),
           data: Joi.any(),
         })
       ),
