c45ff7317a3e00155b0841027cd83c4fcfdf05d2
Ofir Sudai <ofir@unevenlabs.com>
2022-12-20 12:17:39 -0500

    fix: diabled type config

--- src/common/redshift.ts
+++ src/common/redshift.ts
@@ -4,9 +4,6 @@ import { config } from "@/config/index";
 
 export const rsp = PgPromise();
 
-// https://github.com/vitaly-t/pg-promise/issues/130
-rsp.pg.types.setTypeParser(1114, (s) => s);
-
 export const redshift = config.redshiftUrl
   ? rsp({
       connectionString: config.redshiftUrl,
