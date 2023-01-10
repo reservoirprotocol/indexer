c39d7a5228f58434f4d2b457941bbc45d6704600
nofir <ofirsud@gmail.com>
2023-01-10 13:12:37 -0500

    fix: fixed migration

--- src/migrations/1673372449340_nft-transfers-created-at.sql
+++ src/migrations/1673372449340_nft-transfers-created-at.sql
@@ -1,6 +1,7 @@
 -- Up Migration
 
-ALTER TABLE "nft_transfer_events" ADD COLUMN "created_at" TIMESTAMPTZ DEFAULT now();
+ALTER TABLE "nft_transfer_events" ADD COLUMN "created_at" TIMESTAMPTZ;
+ALTER TABLE "nft_transfer_events" ALTER "created_at" SET DEFAULT now();
 
 -- Down Migration
 
