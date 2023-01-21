5bb6a60d9a59ced57428d22c198158cf829959d5
Mihalis Plelis <mihalispl@gmail.com>
2023-01-21 17:24:43 +0200

    fix: fix tokens/v5 sorting

--- src/api/endpoints/tokens/get-tokens/v5.ts
+++ src/api/endpoints/tokens/get-tokens/v5.ts
@@ -730,7 +730,7 @@ export const getTokensV5Options: RouteOptions = {
       // Sorting
 
       // Only allow sorting on floorSell when we filter by collection / attributes / tokenSetId / rarity
-      if (query.collection || query.attributes || query.tokenSetId || query.rarity) {
+      if (query.collection || query.attributes || query.tokenSetId || query.rarity || query.tokens) {
         switch (query.sortBy) {
           case "rarity": {
             baseQuery += ` ORDER BY t.rarity_rank ${
@@ -758,7 +758,7 @@ export const getTokensV5Options: RouteOptions = {
             break;
           }
         }
-      } else if (query.contract || query.tokens) {
+      } else if (query.contract) {
         baseQuery += ` ORDER BY t.token_id ${query.sortDirection || "ASC"}`;
       }
 
