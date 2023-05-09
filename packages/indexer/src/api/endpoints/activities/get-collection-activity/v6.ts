c98d32f87e8d3add594ccc4f32826872d9eb414a
Jamie Chicago <87397251+jamiechicago312@users.noreply.github.com>
2023-05-09 12:25:55 -0500

    feat: response "price" description
    Price will "Return native currency unless displayCurrency contract was passed."

--- packages/indexer/src/api/endpoints/activities/get-collection-activity/v6.ts
+++ packages/indexer/src/api/endpoints/activities/get-collection-activity/v6.ts
@@ -101,7 +101,7 @@ export const getCollectionActivityV6Options: RouteOptions = {
           type: Joi.string(),
           fromAddress: Joi.string(),
           toAddress: Joi.string().allow(null),
-          price: JoiPrice.allow(null),
+          price: JoiPrice.allow(null).description("Return native currency unless displayCurrency contract was passed."),
           amount: Joi.number().unsafe(),
           timestamp: Joi.number(),
           createdAt: Joi.string(),
