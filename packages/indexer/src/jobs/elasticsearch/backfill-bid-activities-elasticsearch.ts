5f791d9ab5eead83b2c703e6db131f573767d15f
nofir <ofirsud@gmail.com>
2023-05-10 17:28:32 -0400

    feat: added bid / bid cancel

--- packages/indexer/src/jobs/elasticsearch/backfill-bid-activities-elasticsearch.ts
+++ packages/indexer/src/jobs/elasticsearch/backfill-bid-activities-elasticsearch.ts
@@ -26,7 +26,7 @@ export const queue = new Queue(QUEUE_NAME, {
 new QueueScheduler(QUEUE_NAME, { connection: redis.duplicate() });
 
 // BACKGROUND WORKER ONLY
-if (config.doBackgroundWork) {
+if (config.doBackgroundWork && config.doElasticsearchWork) {
   const worker = new Worker(
     QUEUE_NAME,
     async (job: Job) => {
