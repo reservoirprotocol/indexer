fcfb6e43ca00c4fc0f30ca73e3711a117a8d4421
nofir <ofirsud@gmail.com>
2023-05-12 00:16:11 -0400

    feat: fixed search

--- packages/indexer/src/jobs/elasticsearch/backfill-transfer-activities-elasticsearch.ts
+++ packages/indexer/src/jobs/elasticsearch/backfill-transfer-activities-elasticsearch.ts
@@ -106,7 +106,7 @@ if (config.doBackgroundWork && config.doElasticsearchWork) {
   });
 
   redlock
-    .acquire([`${QUEUE_NAME}-lock-v7`], 60 * 60 * 24 * 30 * 1000)
+    .acquire([`${QUEUE_NAME}-lock-v8`], 60 * 60 * 24 * 30 * 1000)
     .then(async () => {
       await addToQueue();
     })
