5c460d42c29849ba24f2165d2f5b6d17c74d6138
Jamie Chicago <87397251+jamiechicago312@users.noreply.github.com>
2023-05-15 15:02:54 -0500

    feat: added response descriptions
    Added response descriptions:
    - quanitity remaining
    - criteria
    - txTimestamp
    - createAt

--- packages/indexer/src/api/endpoints/events/get-asks-events/v3.ts
+++ packages/indexer/src/api/endpoints/events/get-asks-events/v3.ts
@@ -75,7 +75,7 @@ export const getAsksEventsV3Options: RouteOptions = {
             contract: Joi.string().lowercase().pattern(regex.address),
             maker: Joi.string().lowercase().pattern(regex.address).allow(null),
             price: JoiPrice.allow(null),
-            quantityRemaining: Joi.number().unsafe(),
+            quantityRemaining: Joi.number().unsafe().description("With ERC1155s, quantity can be higher than 1"),
             nonce: Joi.string().pattern(regex.number).allow(null),
             validFrom: Joi.number().unsafe().allow(null),
             validUntil: Joi.number().unsafe().allow(null),
@@ -83,7 +83,7 @@ export const getAsksEventsV3Options: RouteOptions = {
             kind: Joi.string(),
             source: Joi.string().allow("", null),
             isDynamic: Joi.boolean(),
-            criteria: JoiOrderCriteria.allow(null),
+            criteria: JoiOrderCriteria.allow(null).description("`kind` can return `token`, `collection`, or `attribute`."),
           }),
           event: Joi.object({
             id: Joi.number().unsafe(),
@@ -99,8 +99,8 @@ export const getAsksEventsV3Options: RouteOptions = {
               "reprice"
             ),
             txHash: Joi.string().lowercase().pattern(regex.bytes32).allow(null),
-            txTimestamp: Joi.number().allow(null),
-            createdAt: Joi.string(),
+            txTimestamp: Joi.number().allow(null).description("Time when added on the blockchain."),
+            createdAt: Joi.string().description("Time when added to indexer")
           }),
         })
       ),
