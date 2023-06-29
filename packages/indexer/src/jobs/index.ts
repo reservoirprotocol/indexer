1fed007a053c13ce0399f3f13bafb14e90756ce9
Idan Peleg <ipeleg@hotmail.com>
2023-06-29 00:02:54 -0400

    feat: enable queues

--- packages/indexer/src/jobs/index.ts
+++ packages/indexer/src/jobs/index.ts
@@ -167,12 +167,12 @@ import { processArchiveDataJob } from "@/jobs/data-archive/process-archive-data-
 import { exportDataJob } from "@/jobs/data-export/export-data-job";
 import { processActivityEventJob } from "@/jobs/activities/process-activity-event-job";
 import { savePendingActivitiesJob } from "@/jobs/activities/save-pending-activities-job";
-// import { eventsSyncFtTransfersWriteBufferJob } from "@/jobs/events-sync/write-buffers/ft-transfers-job";
-// import { eventsSyncNftTransfersWriteBufferJob } from "@/jobs/events-sync/write-buffers/nft-transfers-job";
-// import { eventsSyncProcessBackfillJob } from "@/jobs/events-sync/process/events-sync-process-backfill";
-// import { openseaBidsQueueJob } from "@/jobs/orderbook/opensea-bids-queue-job";
-// import { processResyncRequestJob } from "@/jobs/events-sync/process-resync-request-queue-job";
-// import { eventsSyncBackfillJob } from "@/jobs/events-sync/events-sync-backfill-job";
+import { eventsSyncFtTransfersWriteBufferJob } from "@/jobs/events-sync/write-buffers/ft-transfers-job";
+import { eventsSyncNftTransfersWriteBufferJob } from "@/jobs/events-sync/write-buffers/nft-transfers-job";
+import { eventsSyncProcessBackfillJob } from "@/jobs/events-sync/process/events-sync-process-backfill";
+import { openseaBidsQueueJob } from "@/jobs/orderbook/opensea-bids-queue-job";
+import { processResyncRequestJob } from "@/jobs/events-sync/process-resync-request-queue-job";
+import { eventsSyncBackfillJob } from "@/jobs/events-sync/events-sync-backfill-job";
 import { blockCheckJob } from "@/jobs/events-sync/block-check-queue-job";
 import { collectionNormalizedJob } from "@/jobs/collection-updates/collection-normalized-floor-queue-job";
 import { replaceActivitiesCollectionJob } from "@/jobs/activities/replace-activities-collection-job";
@@ -337,12 +337,12 @@ export class RabbitMqJobsConsumer {
       exportDataJob,
       processActivityEventJob,
       savePendingActivitiesJob,
-      // eventsSyncFtTransfersWriteBufferJob,
-      // eventsSyncNftTransfersWriteBufferJob,
-      // eventsSyncProcessBackfillJob,
-      // openseaBidsQueueJob,
-      // processResyncRequestJob,
-      // eventsSyncBackfillJob,
+      eventsSyncFtTransfersWriteBufferJob,
+      eventsSyncNftTransfersWriteBufferJob,
+      eventsSyncProcessBackfillJob,
+      openseaBidsQueueJob,
+      processResyncRequestJob,
+      eventsSyncBackfillJob,
       blockCheckJob,
       collectionNormalizedJob,
       replaceActivitiesCollectionJob,
