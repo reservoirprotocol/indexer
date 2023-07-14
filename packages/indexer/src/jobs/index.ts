9e841e41e2bd2396d0bfa8ac2d5d8f3ba9ec2461
nofir <ofirsud@gmail.com>
2023-07-14 16:31:30 -0400

    feat: added basic job

--- packages/indexer/src/jobs/index.ts
+++ packages/indexer/src/jobs/index.ts
@@ -91,6 +91,8 @@ import { exportDataJob } from "@/jobs/data-export/export-data-job";
 import { processActivityEventJob } from "@/jobs/activities/process-activity-event-job";
 import { savePendingActivitiesJob } from "@/jobs/activities/save-pending-activities-job";
 import { deleteArchivedExpiredBidActivitiesJob } from "@/jobs/activities/delete-archived-expired-bid-activities-job";
+import { reindexActivitiesJob } from "@/jobs/activities/reindex-activities-job";
+
 import { eventsSyncFtTransfersWriteBufferJob } from "@/jobs/events-sync/write-buffers/ft-transfers-job";
 import { eventsSyncNftTransfersWriteBufferJob } from "@/jobs/events-sync/write-buffers/nft-transfers-job";
 import { eventsSyncProcessBackfillJob } from "@/jobs/events-sync/process/events-sync-process-backfill";
@@ -264,6 +266,7 @@ export class RabbitMqJobsConsumer {
       openseaOrdersFetchJob,
       saveBidEventsJob,
       countApiUsageJob,
+      reindexActivitiesJob,
     ];
   }
 
