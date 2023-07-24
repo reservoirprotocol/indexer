16775509e089f4683adceb45c3ed66dc1714f8d7
nofir <ofirsud@gmail.com>
2023-07-24 15:32:35 -0400

    feat: added logs

--- packages/indexer/src/index.ts
+++ packages/indexer/src/index.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+
 import { config as dotEnvConfig } from "dotenv";
 dotEnvConfig();
 
@@ -10,16 +12,34 @@ import { config } from "@/config/index";
 if (process.env.LOCAL_TESTING) {
   import("./setup");
 } else {
+  console.log("Connecting to RabbitMQ");
+
   RabbitMq.connect().then(async () => {
+    console.log("Connected to RabbitMQ");
+
     // Sync the pods so rabbit queues assertion will run only once per deployment by a single pod
     if (await acquireLock(config.imageTag, 75)) {
+      console.log("acquired lock");
+
       await RabbitMq.assertQueuesAndExchanges();
+
+      console.log("queues asserted");
+
       await redis.set(config.imageTag, "DONE", "EX", 60 * 60 * 24); // Update the lock ttl
+
+      console.log("lock set");
+
       import("./setup");
     } else {
+      console.log("unable to acquire lock");
+
       // Check every 1s if the rabbit queues assertion completed
       const intervalId = setInterval(async () => {
+        console.log("interval - checking lock");
+
         if ((await redis.get(config.imageTag)) === "DONE") {
+          console.log("clearing interval");
+
           clearInterval(intervalId);
           import("./setup");
         }
