65f6db0eb370fab4ad019cfded46156671fa03fe
d3or <deorrunner@gmail.com>
2023-08-01 09:30:20 -0400

    feat: update converstions tx log insert

--- packages/indexer/src/models/transaction-logs/index.ts
+++ packages/indexer/src/models/transaction-logs/index.ts
@@ -2,50 +2,58 @@ import { Log } from "@ethersproject/abstract-provider";
 
 import { pgp, txdb } from "@/common/db";
 import { toBuffer } from "@/common/utils";
+import { logger } from "@/common/logger";
 export type TransactionLogs = {
   hash: string;
   logs: Log[];
 };
 export const saveTransactionLogs = async (transactionLogs: Log[]) => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const values: any[] = [];
-  const columns = new pgp.helpers.ColumnSet(
-    [
-      "hash",
-      "address",
-      "block_number",
-      "block_hash",
-      "transaction_index",
-      "log_index",
-      "data",
-      "topics",
-      "removed",
-    ],
-    {
-      table: "transaction_logs",
+  try {
+    if (!transactionLogs || transactionLogs.length === 0) {
+      return;
     }
-  );
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const values: any[] = [];
+    const columns = new pgp.helpers.ColumnSet(
+      [
+        "hash",
+        "address",
+        "block_number",
+        "block_hash",
+        "transaction_index",
+        "log_index",
+        "data",
+        {
+          name: "topics",
+          cast: "bytea[]",
+        },
+        "removed",
+      ],
+      {
+        table: "transaction_logs",
+      }
+    );
 
-  for (const log of transactionLogs) {
-    values.push({
-      hash: toBuffer(log.transactionHash),
-      address: log.address,
-      block_number: log.blockNumber,
-      block_hash: log.blockHash,
-      transaction_index: log.transactionIndex,
-      log_index: log.logIndex,
-      data: log.data,
-      topics: log.topics,
-      removed: log.removed,
-    });
-  }
+    for (const log of transactionLogs) {
+      values.push({
+        hash: toBuffer(log.transactionHash),
+        address: toBuffer(log.address),
+        block_number: log.blockNumber,
+        block_hash: toBuffer(log.blockHash),
+        transaction_index: log.transactionIndex,
+        log_index: log.logIndex,
+        data: toBuffer(log.data),
+        topics: log.topics,
+        removed: log.removed ? true : false,
+      });
+    }
 
-  if (values.length === 0) {
-    return;
-  }
+    if (values.length === 0) {
+      return;
+    }
 
-  await txdb.none(
-    `
+    await txdb.none(
+      `
       INSERT INTO "transaction_logs" (
         hash,
         address,
@@ -60,9 +68,12 @@ export const saveTransactionLogs = async (transactionLogs: Log[]) => {
       ) VALUES ${pgp.helpers.values(values, columns)}
       ON CONFLICT DO NOTHING  
     `
-  );
+    );
 
-  return transactionLogs;
+    return transactionLogs;
+  } catch (error) {
+    logger.error("save-transaction-logs", `Error saving transaction logs: ${error}`);
+  }
 };
 
 export const getTransactionLogs = async (hash: string): Promise<TransactionLogs> => {
