20acd6064de260e14b0fdeaa21b92f49a6181886
d3or <deorrunner@gmail.com>
2023-08-25 05:57:45 -0400

    feat: disable tracer to test for memory leak

--- packages/indexer/src/common/tracer.ts
+++ packages/indexer/src/common/tracer.ts
@@ -1,23 +1,31 @@
-import tracer from "dd-trace";
-import { getServiceName } from "@/config/network";
-import { config } from "@/config/index";
+// import tracer from "dd-trace";
+// import { getServiceName } from "@/config/network";
+// import { config } from "@/config/index";
 
-if (process.env.DATADOG_AGENT_URL) {
-  const service = getServiceName();
+// if (process.env.DATADOG_AGENT_URL && config.chainId !== 5) {
+//   const service = getServiceName();
 
-  tracer.init({
-    profiling: true,
-    logInjection: true,
-    runtimeMetrics: true,
-    clientIpEnabled: true,
-    service,
-    url: process.env.DATADOG_AGENT_URL,
-    env: config.environment,
-  });
+//   tracer.init({
+//     profiling: true,
+//     logInjection: true,
+//     runtimeMetrics: true,
+//     clientIpEnabled: true,
+//     service,
+//     url: process.env.DATADOG_AGENT_URL,
+//     env: config.environment,
+//   });
 
-  tracer.use("hapi", {
-    headers: ["x-api-key", "referer"],
-  });
-}
+//   tracer.use("hapi", {
+//     headers: ["x-api-key", "referer"],
+//   });
+// }
+/* eslint-disable */
+
+const tracer: any = {
+  trace: () => {},
+  use: () => {},
+  init: () => {},
+  scope: () => {},
+};
 
 export default tracer;
