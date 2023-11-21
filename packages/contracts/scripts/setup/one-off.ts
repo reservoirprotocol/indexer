5e73fdf178c371bb517ef00e13a4ba7b28ca7335
forward <qq1506430045@163.com>
2023-11-21 17:41:37 +0800

    x1 testnet

--- packages/contracts/scripts/setup/one-off.ts
+++ packages/contracts/scripts/setup/one-off.ts
@@ -2,22 +2,19 @@
 
 import { ethers } from "hardhat";
 
-import { DEPLOYER, readDeployment, trigger } from "./trigger";
+import { DEPLOYER, trigger } from "./trigger";
 
 const main = async () => {
   const chainId = await ethers.provider.getNetwork().then((n) => n.chainId);
-  //
-  // // Make sure the current signer is the canonical deployer
-  // const [deployer] = await ethers.getSigners();
-  // if (deployer.address.toLowerCase() !== DEPLOYER.toLowerCase()) {
-  //   throw new Error("Wrong deployer");
-  // }
-  //
-  // chainId;
-  // trigger;
-    // 部署 AlienswapModule
-    const router = await trigger.Router.ApprovalProxy(chainId)
-    console.log("xxxxxx", router)
+
+  // Make sure the current signer is the canonical deployer
+  const [deployer] = await ethers.getSigners();
+  if (deployer.address.toLowerCase() !== DEPLOYER.toLowerCase()) {
+    throw new Error("Wrong deployer");
+  }
+
+  chainId;
+  trigger;
 };
 
 main()
