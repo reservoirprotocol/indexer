e0323de00e11c5f9100f04624d72040fa4ccc99a
nofir <ofirsud@gmail.com>
2023-12-07 15:09:22 -0500

    feat: fix query

--- packages/indexer/src/api/endpoints/search/get-search-collections/v2.ts
+++ packages/indexer/src/api/endpoints/search/get-search-collections/v2.ts
@@ -6,7 +6,7 @@ import Joi from "joi";
 
 import { logger } from "@/common/logger";
 import { redb } from "@/common/db";
-import { formatEth, fromBuffer, now, regex } from "@/common/utils";
+import { formatEth, fromBuffer, now, regex, toBuffer } from "@/common/utils";
 import { CollectionSets } from "@/models/collection-sets";
 import { Assets } from "@/utils/assets";
 import { getUSDAndCurrencyPrices } from "@/utils/prices";
@@ -88,6 +88,7 @@ export const getSearchCollectionsV2Options: RouteOptions = {
 
     if (query.name) {
       if (isAddress(query.name)) {
+        query.name = toBuffer(query.name);
         conditions.push(`c.contract = $/name/`);
       } else {
         query.name = `%${query.name}%`;
