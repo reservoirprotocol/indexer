d637262811dd01cee363d540e44a3277847cbff2
Ruijian Cao <caoruijian@big.one>
2023-12-16 14:27:52 +0800

    fix: get router addr

--- packages/contracts/scripts/setup/one-off.ts
+++ packages/contracts/scripts/setup/one-off.ts
@@ -13,9 +13,10 @@ const main = async () => {
     throw new Error("Wrong deployer");
   }
 
-  // const router = await trigger.Router.V6_0_1();
-  // // 输出日志
-  // // Version v3 of contract ReservoirV6_0_1 deployed on chain 195 at address 0x9f7292ba383db44928933f52ef2acc03816594f2
+  const router = await trigger.Router.V6_0_1();
+  // 输出日志
+  // Version v3 of contract ReservoirV6_0_1 deployed on chain 195 at address 0x9f7292ba383db44928933f52ef2acc03816594f2
+  console.log(`router output: ${router}`);
 
   // // 部署 AlienswapModule
   // const address = await trigger.Modules.AlienswapModule(chainId);
@@ -26,9 +27,6 @@ const main = async () => {
   // const router = await trigger.Router.ApprovalProxy(chainId)
   // // 输出日志
   // // Version v1 of contract ReservoirApprovalProxy deployed on chain 195 at address 0x6072b1def04135d572ee645437ac0be6f3e0b980
-
-  chainId;
-  trigger;
 };
 
 main()
