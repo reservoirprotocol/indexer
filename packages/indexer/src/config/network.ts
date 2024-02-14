4562d271a7b0feeb36a08fe2a6dca207e7bc066d
Nikunj goyal <40403668+Nike682631@users.noreply.github.com>
2024-02-15 01:54:00 +0530

    remove unnecessary formatting

--- packages/indexer/src/config/network.ts
+++ packages/indexer/src/config/network.ts
@@ -151,8 +151,9 @@ export const getServiceName = () => {
 };
 
 export const getSubDomain = () => {
-  return `${config.chainId === 1 ? "api" : `api-${getNetworkName()}`}${config.environment === "dev" ? ".dev" : ""
-    }`;
+  return `${config.chainId === 1 ? "api" : `api-${getNetworkName()}`}${
+    config.environment === "dev" ? ".dev" : ""
+  }`;
 };
 
 type NetworkSettings = {
