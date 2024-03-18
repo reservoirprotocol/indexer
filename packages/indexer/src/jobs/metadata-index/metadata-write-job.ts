84fca3d899f52521fbe2210be257c07dc973e324
nofir <ofirsud@gmail.com>
2024-03-18 10:44:37 -0400

    feat: added fallback limit logic

--- packages/indexer/src/jobs/metadata-index/metadata-write-job.ts
+++ packages/indexer/src/jobs/metadata-index/metadata-write-job.ts
@@ -111,24 +111,33 @@ export default class MetadataIndexWriteJob extends AbstractRabbitMqJobHandler {
     }
 
     if (metadataMethod === "simplehash") {
-      const debugSimplehashFallbackError = await redis.hget(
+      const fallbackError = await redis.hget(
         "simplehash-fallback-debug-tokens-v2",
         `${contract}:${tokenId}`
       );
 
-      if (debugSimplehashFallbackError) {
+      if (fallbackError) {
+        const fallbackSuccess = name != null || imageUrl != null;
+
         logger.info(
           this.queueName,
           JSON.stringify({
             topic: "simpleHashFallbackDebug",
             message: `Fallback. collection=${collection}, tokenId=${tokenId}`,
             payload,
-            fallbackSuccess: name != null || imageUrl != null,
-            fallbackError: debugSimplehashFallbackError,
+            fallbackSuccess,
+            fallbackError,
           })
         );
 
         await redis.hdel("simplehash-fallback-debug-tokens-v2", `${contract}:${tokenId}`);
+
+        if (!fallbackSuccess) {
+          const redisMulti = redis.multi();
+          await redisMulti.incr(`simplehash-fallback-failures:${contract}`);
+          await redisMulti.expire(`simplehash-fallback-failures:${contract}`, 3600);
+          await redisMulti.exec();
+        }
       }
     }
 
