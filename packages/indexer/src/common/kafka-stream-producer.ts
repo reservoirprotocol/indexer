f2d7ae91494c4208b4d66f951803db71f7dfceed
nofir <ofirsud@gmail.com>
2024-03-19 16:39:44 -0400

    feat: fix log

--- packages/indexer/src/common/kafka-stream-producer.ts
+++ packages/indexer/src/common/kafka-stream-producer.ts
@@ -36,7 +36,7 @@ export async function start(): Promise<void> {
 
   try {
     await producer.connect();
-    logger.info(`kafka-producer`, "Producer connected");
+    logger.info(`kafka-stream-producer`, "Producer connected");
   } catch (error) {
     logger.error(
       `kafka-stream-producer`,
